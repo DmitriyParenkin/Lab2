a2450c5ae2ee032ba565e5ef18eabd82d836abd7
Парёнкин Дмитрий Александрович <ip611s17@awp10.csc.local>
2017-03-01 12:52:43 +0700

    Input

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+int sum, time, pers;
+printf("Введите сумму вклада "" ");
+scanf("%d", &sum);
+printf("Введите срок вклада "" ");
+scanf("%d", &time);
+ 
+}
